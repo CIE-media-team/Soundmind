1d0e166f21450c510a7d1c2cf861622ee8067a4a
Kinsley Sigmund <kinsley.sigmund@gmail.com>
2019-03-14 20:16:56 -0400

    My Calendar UI, add button, needs further functionality

--- app/src/main/java/com/hfad/soundmind/MainActivity.java
+++ app/src/main/java/com/hfad/soundmind/MainActivity.java
@@ -1,8 +1,16 @@
 package com.hfad.soundmind;
 
+import android.content.Context;
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,5 +22,29 @@ public class MainActivity extends AppCompatActivity {
 
     public void onClickMyCalendarFAB(View view) {
         setContentView(R.layout.activity_calendar);
+    }
+    public void onClickAddToDo(View view) {
+        final EditText taskEditText = new EditText(this);
+        AlertDialog dialog = new AlertDialog.Builder(this)
+                .setTitle("Add a new task")
+                .setMessage("What do you want to do next?")
+                .setView(taskEditText)
+                .setPositiveButton("Add", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        String task = String.valueOf(taskEditText.getText());
+                        createTask(task);
+                    }
+                })
+                .setNegativeButton("Cancel", null)
+                .create();
+        dialog.show();
+    }
+
+    public void createTask(String task){
+        LinearLayout monthly_todo_container = findViewById(R.id.monthly_todo_list);
+        CheckBox newTask = new CheckBox(getApplicationContext());
+        newTask.setText(task);
+        monthly_todo_container.addView(newTask);
     }
 }
